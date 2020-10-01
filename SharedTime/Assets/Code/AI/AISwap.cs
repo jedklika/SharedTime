e14baaf0d77ba73a4bccc5d3e1bb67411a359d18
jedklika <43482414+jedklika@users.noreply.github.com>
2020-10-01 02:49:38 -0400

    Intial push
    Contains AI code, and a game manager with damage stuff. Also the editor has folders to better organize things for us

---
+++ SharedTime/Assets/Code/AI/AISwap.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISwap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool Right;
+    //public SpriteRenderer LightFoe;
+    //public bool flipped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInParent<Patrol>().enabled = true;
+        GetComponentInParent<Attack>().enabled = false;
+        //LightFoe = GetComponentInParent<SpriteRenderer>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Right)
+        {
+            Debug.Log("Kill");
+            GetComponentInParent<Attack>().enabled = true;
+            GetComponentInParent<Patrol>().enabled = false;
+            //LightFoe.flipX = true;
+            //flipped = true;
+        }
+        if (collision.gameObject.CompareTag("Player") && Right == false)
+        {
+            Debug.Log("Kill");
+            GetComponentInParent<Attack>().enabled = true;
+            GetComponentInParent<Patrol>().enabled = false;
+            //LightFoe.flipX = false;
+            //flipped = false;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Right)
+        {
+            Debug.Log("Kill");
+            GetComponentInParent<Attack>().enabled = true;
+            GetComponentInParent<Patrol>().enabled = false;
+        }
+        if (collision.gameObject.CompareTag("Player") && Right == false)
+        {
+            Debug.Log("Kill");
+            GetComponentInParent<Attack>().enabled = true;
+            GetComponentInParent<Patrol>().enabled = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Right)
+        {
+            Debug.Log("Partol");
+            GetComponentInParent<Patrol>().enabled = true;
+            GetComponentInParent<Attack>().enabled = false;
+            //LightFoe.flipX = false;
+        }
+        if (collision.gameObject.CompareTag("Player") && Right == false)
+        {
+            Debug.Log("Partol");
+            GetComponentInParent<Patrol>().enabled = true;
+            GetComponentInParent<Attack>().enabled = false;
+            //LightFoe.flipX = true;
+        }
+    }
+}
---
+++ SharedTime/Assets/Code/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public float playerHealth = 90;
+	public float playerMaxHealth = 100;
+
+	public float SprintTime = 30;
+	public bool sprint;
+	private bool exhausted = false;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	public bool CanSprint()
+	{
+		//Recover a bit if exhausted
+		if (exhausted)
+		{
+			if (SprintTime > 10.0f)
+				exhausted = false;
+
+			return false;
+		}
+		else
+		{
+			if (SprintTime < 1.0f)
+				exhausted = true;
+
+			return true;
+		}
+	}
+	public void setDamage(float new_damage)
+	{
+		playerHealth -= new_damage;
+
+	}
+}
