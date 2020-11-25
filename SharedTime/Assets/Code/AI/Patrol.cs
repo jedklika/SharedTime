8561a033acc8f5e03d0873a682b29a805d7b2bfe
dprefontaine <dprefontaine1@student.gsu.edu>
2020-11-24 22:52:42 -0500

    t

--- SharedTime/Assets/Code/AI/Patrol.cs
+++ SharedTime/Assets/Code/AI/Patrol.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    [SerializeField]
-    Transform[] waypoints;
+    //[SerializeField]
+
     public float speed;
     public bool canFlip;
     public bool FoeFlipped;
@@ -21,18 +21,20 @@ public class Patrol : MonoBehaviour
 	//USE THIS FOR AI PATHFINDING
 	public BoxCollider2D patrolField;
 	public float field_cut_rate;
-	public float x_field_offset, y_field_offset;
 	
 	//THESE SHOULD BE CREATED FROM PATROL FIELD
 	private int[,] patrol_grid;   //INT VALUES ARE THE COLORS
 	private int patrol_grid_x_size;
 	private int patrol_grid_y_size;
 	
+	private int self_index_x;
+	private int self_index_y;
+	
     // Start is called before the first frame update
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
-        transform.position = waypoints[waypointIndex].transform.position;
+		
 		patrol_grid = new int[(int)(patrolField.size.x/field_cut_rate),(int)(patrolField.size.y/field_cut_rate)];
 		patrol_grid_x_size = (int)(patrolField.size.x/field_cut_rate);
 		patrol_grid_y_size = (int)(patrolField.size.y/field_cut_rate);
@@ -43,15 +45,9 @@ public class Patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
-        if (transform.position == waypoints[waypointIndex].transform.position)
-        {
-            waypointIndex += 1;
-        }
-        if (waypointIndex == waypoints.Length)
-        {
-            waypointIndex = 0;
-        }
+		updatePatrolGrid();
+		updateMove();
+        
         if (health <= 0)
         {
             gameObject.SetActive(false);
@@ -59,44 +55,70 @@ public class Patrol : MonoBehaviour
     }
 	
 	void updatePatrolGrid(){
+		float patrol_grid_draw_x_iteration = 0, patrol_grid_draw_y_iteration = 0;
+		int x_index = 0, y_index = 0;
 		
+		for (;x_index < patrol_grid_x_size; x_index++){
+			for (;y_index < patrol_grid_y_size; y_index++){
+				float x_spot, y_spot;
+				x_spot = patrol_grid_draw_x_iteration + patrolField.transform.position.x - patrolField.size.x/2;
+				y_spot = patrol_grid_draw_y_iteration + patrolField.transform.position.y - patrolField.size.y/2;
+				
+				//CHECK FOR GROUND BELOW
+				if (checkForGround(x_spot, y_spot)){
+					patrol_grid[x_index, y_index] = 1;
+				} else if (checkForEnemies(x_spot, y_spot)){
+					patrol_grid[x_index, y_index] = 2;
+				} else if (checkForPlayer(x_spot, y_spot)){
+					patrol_grid[x_index, y_index] = 3;
+				} else {
+					patrol_grid[x_index, y_index] = 0;
+				}
+				
+				checkForSelf(x_spot, y_spot, x_index, y_index);
+				
+				patrol_grid_draw_y_iteration+=field_cut_rate;
+			}
+			
+			patrol_grid_draw_y_iteration = 0;
+			y_index = 0;
+			
+			patrol_grid_draw_x_iteration+=field_cut_rate;
+		}
 	}
 	
 	void OnDrawGizmos(){
-		int patrol_grid_draw_x_iteration = -patrol_grid_x_size/2, patrol_grid_draw_y_iteration = -patrol_grid_y_size/2;
+		float patrol_grid_draw_x_iteration = 0, patrol_grid_draw_y_iteration = 0;
 		int x_index = 0, y_index = 0;
 		
-		for (;patrol_grid_draw_x_iteration < patrol_grid_x_size/2; patrol_grid_draw_x_iteration++){
-			for (;patrol_grid_draw_y_iteration < patrol_grid_y_size/2; patrol_grid_draw_y_iteration++){
+		for (;x_index < patrol_grid_x_size; x_index++){
+			for (;y_index < patrol_grid_y_size; y_index++){
 				float x_spot, y_spot;
-				x_spot = patrol_grid_draw_x_iteration + patrolField.transform.position.x + x_field_offset;
-				y_spot = patrol_grid_draw_y_iteration + patrolField.transform.position.y + y_field_offset;
+				x_spot = patrol_grid_draw_x_iteration + patrolField.transform.position.x - patrolField.size.x/2;
+				y_spot = patrol_grid_draw_y_iteration + patrolField.transform.position.y - patrolField.size.y/2;
 				
 				//CHECK FOR GROUND BELOW
 				if (checkForGround(x_spot, y_spot)){
 					Gizmos.color = new Color(0f,0f,0f,0.5f);
-					patrol_grid[x_index, y_index] = 1;
-				} else if (checkForSelf(x_spot, y_spot)){
+				} else if (checkForEnemies(x_spot, y_spot)){
 					Gizmos.color = new Color(1f,0f,0f,0.5f);
-					Debug.Log("Enemy Spot: " + x_index + " " + y_index);
-					patrol_grid[x_index, y_index] = 2;
+					//Debug.Log("Enemy Spot: " + x_index + " " + y_index);
 				} else if (checkForPlayer(x_spot, y_spot)){
 					Gizmos.color = new Color(0f,1f,0f,0.5f);
-					Debug.Log("Player Spot: " + x_index + " " + y_index);
-					patrol_grid[x_index, y_index] = 3;
+					//Debug.Log("Player Spot: " + x_index + " " + y_index);
 				} else {
 					Gizmos.color = new Color(0f,0f,1f,0.5f);
-					patrol_grid[x_index, y_index] = 0;
 				}
 				
 				Gizmos.DrawCube(new Vector3(x_spot, y_spot, 0), new Vector3(field_cut_rate*0.9f,field_cut_rate*0.9f,1.0f));
 				
-				y_index++;
+				patrol_grid_draw_y_iteration+=field_cut_rate;
 			}
 			
-			patrol_grid_draw_y_iteration = -patrol_grid_y_size/2;
+			patrol_grid_draw_y_iteration = 0;
 			y_index = 0;
-			x_index++;
+			
+			patrol_grid_draw_x_iteration+=field_cut_rate;
 		}
 	}
 	
@@ -107,59 +129,59 @@ public class Patrol : MonoBehaviour
 		return (check.collider != null);
 	}
 	
-	bool checkForSelf(float x_to_position, float y_to_position){
-		float self_x_position, self_y_position;
+	bool checkForEnemies(float x_to_raycast, float y_to_raycast){
+		Vector2 ground_spot = new Vector2(x_to_raycast, y_to_raycast);
+		RaycastHit2D check = Physics2D.Raycast(ground_spot, Vector2.zero, 0.0f, LayerMask.GetMask("Enemies"));
 		
-		float x_estimation = field_cut_rate/2f;
-		float y_estimation = field_cut_rate/2f;
+		if (check.collider != null){
+			if (check.collider.CompareTag("Enemy")){
+				return true;
+			}
+		}
 		
-		self_x_position = gameObject.transform.position.x;
-		self_y_position = gameObject.transform.position.y;
-		
-		bool x_test, y_test;
-		
-		x_test = (self_x_position > x_to_position - x_estimation) && (self_x_position < x_to_position + x_estimation);
-		
-		y_test = (self_y_position > y_to_position - y_estimation) && (self_y_position < y_to_position + y_estimation);
-		
-		return x_test && y_test;
+		return false;
 	}
 	
 	//THIS WORKS ABOUT THE SAME AS CHECKING FOR SELF, BUT APPLIED TO PLAYER
-	bool checkForPlayer(float x_to_position, float y_to_position){
-		float self_x_position, self_y_position;
+	bool checkForPlayer(float x_to_raycast, float y_to_raycast){
+		Vector2 ground_spot = new Vector2(x_to_raycast, y_to_raycast);
+		RaycastHit2D check = Physics2D.Raycast(ground_spot, Vector2.zero, 0.0f, LayerMask.GetMask("Default"));
 		
-		float x_estimation = field_cut_rate/2f;
-		float y_estimation = field_cut_rate/2f;
+		if (check.collider != null){
+			return (check.collider.CompareTag("Player"));
+		} 
 		
-		self_x_position = playerPosition.position.x;
-		self_y_position = playerPosition.position.y;
-		
-		bool x_test, y_test;
-		
-		x_test = (self_x_position > x_to_position - x_estimation) && (self_x_position < x_to_position + x_estimation);
-		
-		y_test = (self_y_position > y_to_position - y_estimation) && (self_y_position < y_to_position + y_estimation);
-		
-		return x_test && y_test;
+		return false;
 	}
 	
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Debug.Log("Hitting trigger of tag name " + collision.gameObject.tag);
-
-        if (collision.gameObject.CompareTag("Right") && canFlip)
-        {
-            //transform.localScale = new Vector3(-1f, 1f, 1f);
-            FoeFlipped = S.flipX = true;
-        }
-
-        if (collision.gameObject.CompareTag("Left") && canFlip)
-        {
-            //transform.localScale = new Vector3(1f, 1f, 1f);
-            FoeFlipped = S.flipX =  false;
-        }
-    }
+	void checkForSelf(float x_to_spot, float y_to_spot, int x_index, int y_index){
+		if ((transform.position.x > x_to_spot - field_cut_rate/2 && transform.position.x < x_to_spot + field_cut_rate/2) &&
+			(transform.position.y > y_to_spot - field_cut_rate/2 && transform.position.y < y_to_spot + field_cut_rate/2)){
+			self_index_x = x_index;
+			self_index_y = y_index;
+			
+			Debug.Log(x_index + " " + y_index);
+		}
+	}
+	
+	void updateMove(){
+		if (FoeFlipped){
+			//GO LEFT
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - 5f, transform.position.y), speed*Time.deltaTime);
+			
+			if (patrol_grid[self_index_x-2,self_index_y-2] != 1 ||
+				patrol_grid[self_index_x-2,self_index_y+1] == 1)
+				FoeFlipped = S.flipX = false;
+			
+		} else {
+			//GO RIGHT
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + 5f, transform.position.y), speed*Time.deltaTime);
+			
+			if (patrol_grid[self_index_x+2,self_index_y-2] != 1 ||
+				patrol_grid[self_index_x+2,self_index_y+1] == 1)
+				FoeFlipped = S.flipX = true;
+		}
+	}
    
    public void TakeDamage(int damage)
    {
