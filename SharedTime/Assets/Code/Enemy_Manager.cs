0c48a22425fad84268eee053f9b64cc5aa56dd39
dprefontaine <dprefontaine1@student.gsu.edu>
2020-12-11 15:08:33 -0500

    butts

--- SharedTime/Assets/Code/Enemy_Manager.cs
+++ SharedTime/Assets/Code/Enemy_Manager.cs
@@ -7,6 +7,12 @@ public class Enemy_Manager : MonoBehaviour
 	public List<GameObject> enemy_room_1;
 	public List<GameObject> enemy_room_2;
 	public List<GameObject> enemy_room_3;
+	public List<GameObject> enemy_room_4;
+	public List<GameObject> enemy_room_5;
+	public List<GameObject> enemy_room_6;
+	public List<GameObject> enemy_room_7;
+	public List<GameObject> enemy_room_8;
+	public List<GameObject> enemy_room_9;
 	
     // Start is called before the first frame update
     void Start()
@@ -33,6 +39,24 @@ public class Enemy_Manager : MonoBehaviour
 			case 2:
 				set_enemy_room_three_active();
 			break;
+			case 3:
+				set_enemy_room_four_active();
+			break;
+			case 4:
+				set_enemy_room_five_active();
+			break;
+			case 5:
+				set_enemy_room_six_active();
+			break;
+			case 6:
+				set_enemy_room_seven_active();
+			break;
+			case 7:
+				set_enemy_room_eight_active();
+			break;
+			case 8:
+				set_enemy_room_nine_active();
+			break;
 		}
 	}
 	
@@ -54,6 +78,36 @@ public class Enemy_Manager : MonoBehaviour
 			if (enemy != null)
 				enemy.SetActive(false);
 		}
+		
+		foreach (GameObject enemy in enemy_room_4){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
+		
+		foreach (GameObject enemy in enemy_room_5){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
+		
+		foreach (GameObject enemy in enemy_room_6){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
+		
+		foreach (GameObject enemy in enemy_room_7){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
+		
+		foreach (GameObject enemy in enemy_room_8){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
+		
+		foreach (GameObject enemy in enemy_room_9){
+			if (enemy != null)
+				enemy.SetActive(false);
+		}
 	}
 	
 	void set_enemy_room_one_active(){
@@ -72,6 +126,48 @@ public class Enemy_Manager : MonoBehaviour
 	
 	void set_enemy_room_three_active(){
 		foreach (GameObject enemy in enemy_room_3){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_four_active(){
+		foreach (GameObject enemy in enemy_room_4){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_five_active(){
+		foreach (GameObject enemy in enemy_room_5){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_six_active(){
+		foreach (GameObject enemy in enemy_room_6){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_seven_active(){
+		foreach (GameObject enemy in enemy_room_7){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_eight_active(){
+		foreach (GameObject enemy in enemy_room_8){
+			if (enemy != null)
+				enemy.SetActive(true);
+		}
+	}
+	
+	void set_enemy_room_nine_active(){
+		foreach (GameObject enemy in enemy_room_9){
 			if (enemy != null)
 				enemy.SetActive(true);
 		}
